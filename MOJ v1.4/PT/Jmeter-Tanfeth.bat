5db5835d71e4ff2b4c9447cc9153554536b07583
moustafa85 <moustafa.fci2006@gmail.com>
2021-03-08 06:53:57 +0300

    JMETER-Examples intialization

---
+++ MOJ v1.4/PT/Jmeter-Tanfeth.bat
@@ -0,0 +1,7 @@
+﻿@ECHO OFF
+ECHO ============================
+ECHO Running Scenarios - Tanfeeth
+ECHO ============================
+
+cd C:\MOJ-001\Performance\apache-jmeter-5.2.1\apache-jmeter-5.2.1\bin
+jmeter -n -t C:\MOJ-001\Performance\MOJ\STG-Forums-IssueDecisions.jmx -l C:\MOJ-001\Performance\report-output\Tanfeth-STG-Forums-IssueDecisions-plan.jtl -e -o C:\MOJ-001\Performance\report-output\
---
+++ MOJ v1.4/PT/Jmeter.bat
@@ -0,0 +1,8 @@
+﻿@ECHO OFF
+ECHO ============================
+ECHO Just Running Jmeter through Proxy
+ECHO ============================
+
+cd C:\MOJ-001\Performance\apache-jmeter-5.2.1\apache-jmeter-5.2.1\bin
+jmeter -E https -H 10.179.8.20 -P 8080 -u mmahmed -a Sto26662 -N localhost
+PAUSE
